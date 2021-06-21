70611b1eef7bd2afcdceb710c3c7559df93e3f1f
Jezebel <jezebel@umw.edu>
2021-06-21 13:14:49 -0400

    Change hound to shih-tzu.

--- Doggie.java
+++ Doggie.java
@@ -1,5 +1,5 @@
 
 class Doggie {
     private String name;
-    private int breed; // 1 - golden retriever, 2 - pit bull, 3 - hound
+    private int breed; // 1 - golden retriever, 2 - pit bull, 3 - shih-tzu
 }
